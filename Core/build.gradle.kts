d5007380211b1d7bae63aeedd77a2e34a6bff0f2
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2025-01-04 09:34:09 +0200

    Core module

--- Core/build.gradle.kts
+++ Core/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    id ("kotlin-kapt")
+    id ("dagger.hilt.android.plugin")
 }
 
 android {
@@ -37,6 +39,23 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
+
+    // retrofit
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+
+
+    // coroutines
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+
+
+    //hilt
+    implementation (libs.hilt.android)
+    kapt (libs.hilt.android.compiler)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ Core/src/main/java/com/hamza/core/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.hamza.core.utils
+
+object Constants {
+
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+    const val API_KEY = "fae1d7ec6609e3ff4d32f87766428a03"
+
+    const val CITY_KEY_PREF = "city"
+
+
+}
