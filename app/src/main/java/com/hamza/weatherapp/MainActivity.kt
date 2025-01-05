e13a0f94114101691c2d0191524ee8749cef468e
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2025-01-05 15:52:23 +0200

    delete useless imports

--- app/src/main/java/com/hamza/weatherapp/MainActivity.kt
+++ app/src/main/java/com/hamza/weatherapp/MainActivity.kt
@@ -3,15 +3,6 @@ package com.hamza.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.hamza.cityinput.presentation.CityInputScreen
 import com.hamza.weatherapp.navigation.AppNavigation
 import com.hamza.weatherapp.ui.theme.WeatherAppTheme
 import dagger.hilt.android.AndroidEntryPoint
